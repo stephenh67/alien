b7e40191967d9682a121e3bf645103fa41afdb1a
stephenh67 <sbhope67@gmail.com>
2020-04-09 09:11:28 -0400

    add update screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,12 +21,8 @@ def run_game():
     # Start the main loop for the game.
     while True:
         gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
 
 run_game()
