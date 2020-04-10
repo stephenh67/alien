c36b0112c0900526eec76755289bcf573a50b4ee
stephenh67 <sbhope67@gmail.com>
2020-04-10 11:16:40 -0400

    add collisions

--- game_functions.py
+++ game_functions.py
@@ -58,7 +58,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -68,6 +68,9 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
            bullets.remove(bullet)
 
+    # Check for any bullets that have hit aliens.
+    # If so, get rid of the bullet and the alien.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit un a row"""
@@ -115,6 +118,7 @@ def check_fleet_edges(ai_settings, aliens):
         if alien.check_edges():
             change_fleet_direction(ai_settings, aliens)
             break
+
 
 def change_fleet_direction(ai_settings, aliens):
     """Drop the entire fleet and change the fleet's direction."""
