409dffe2e930480f648d4313d550e5864ff342f8
stephenh67 <sbhope67@gmail.com>
2020-04-10 15:05:05 -0400

    score all hits

--- settings.py
+++ settings.py
@@ -23,6 +23,8 @@ class Settings():
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -34,3 +36,12 @@ class Settings():
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
